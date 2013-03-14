91b6a82b474151267c1c1b20f5d274b1d1bb9c10
charles <cclego@gmail.com>
2013-03-14 00:42:52 +0000

    cache is now thread safe, removed unused code

--- datasets/cache.py
+++ datasets/cache.py
@@ -3,6 +3,8 @@ import pickle
 import os
 import os.path
 import subprocess
+import fcntl
+import time
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 import zipfile
@@ -26,9 +28,6 @@ class Cache:
     self.aws_access_key = self.config['cache']['AWS_ACCESS_KEY']
     self.aws_secret_key = self.config['cache']['AWS_SECRET_KEY']
     self.size = parseSize(self.config['cache']['size'])
-    self.entries = {}
-    self.entrylog = open(self.path+'/cache.log', "a") 
-    self._loadlog()
 
   def s3listcontents(self, bucketname):
     conn = S3Connection(self.aws_access_key, self.aws_secret_key)
@@ -38,15 +37,21 @@ class Cache:
     return o
   
   def s3tocache(self, bucketname, objname, decompress=None):
-    conn = S3Connection(self.aws_access_key, self.aws_secret_key)
-    b = conn.get_bucket(bucketname)
-    k = Key(b)
-    k.key = objname
-    path = storage_name(self.path, objname)
-    k.get_contents_to_filename(path)
-    if decompress is not None:
-      self.decompress(decompress, path) 
-  
+    r = self.__lockOrNone(bucketname, objname)
+    if r is None:
+      while (self.__getStateFromLog(bucketname, objname) == "downloading..."):
+        time.sleep(1)
+    else:
+      conn = S3Connection(self.aws_access_key, self.aws_secret_key)
+      b = conn.get_bucket(bucketname)
+      k = Key(b)
+      k.key = objname
+      path = storage_name(self.path, objname)
+      k.get_contents_to_filename(path)
+      if decompress is not None:
+        self.decompress(decompress, path) 
+      self.__logWithLock(bucketname, objname, "COMPLETE")
+       
   def decompress(self, cmd, path):
     decompath = decompress_name(path)
     if (os.path.isfile(decompath)):
@@ -67,85 +72,48 @@ class Cache:
       path = storage_name(self.path, objname)
     else:
       path = decompress_name(storage_name(self.path, objname))
-    if os.path.isfile(path):
+    if os.path.isfile(path) and self.__getStateFromLog(bucketname, objname) == "COMPLETE":
       return open(path)
     else:
       self.s3tocache(bucketname, objname, decompress=decompress)
       return open(path)
-
-  def store(self, groupname, id, obj):
-    if isinstance(id, int):
-      path = '%s/%s-%d.obj' % (self.path, groupname, id)
-    elif isinstance(id, tuple):
-      (start, end) = id
-      path = '%s/%s-(%d,%d)' % (self.path, groupname, start, end)
-    else:
-      raise Exception("cache only stores individual objs and intervals")    
-    if groupname not in self.entries:
-      self.entries[groupname] = [id]
-    else:
-      self.entries[groupname].append(id) 
-    f = open(path, "w")
-    f.write(pickle.dumps(obj))
-    f.close() 
-    self._log(groupname, id)
-
-  def _loadlog(self):
-    f = open(self.path+'/cache.log', 'r')
-    for l in f.readlines():
-      pieces = l.split(' ')
-      if len(pieces) == 2:
-        if pieces[0] not in self.entries:
-          self.entries[pieces[0]] = []
-        self.entries[pieces[0]].append(int(pieces[1]))
-      elif len(pieces) == 3:
-        if pieces[0] not in self.entries:
-          self.entries[pieces[0]] = []
-        self.entries[pieces[0]].append((int(pieces[1]), int(pieces[2]))) 
-      else:
-        raise Exception('Invalid cache.log')
-    f.close()
-
-  def _log(self, groupname, identifier):
-    if isinstance(identifier, int):
-      entry = '%s %d\n' % (groupname, identifier)
-    elif isinstance(identifier, tuple):
-      (start, end) = identifier
-      entry = '%s %d %d\n' % (groupname, start, end)
-    else:
-      raise Exception("Not proper log entry") 
-    self.entrylog.write(entry)
   
-  def load(self, groupname, id):
-    if groupname not in self.entries:
-      return None
-    if isinstance(id, int): 
-      for i in self.entries[groupname]:
-        if isinstance(i, tuple):
-          (start, end) = i
-          if id >= start and id <= end:
-            path = '%s/%s-(%d,%d)' % (self.path, groupname, start, end)   
-            f = open(path, "r")
-            d = pickle.loads(f.read())
-            f.close()
-            return d[id - start] 
+  def __getStateFromLog(self, bucketname, objname):
+    foundDownload = False
+    entrylog = open(self.path+'/cache.log', "a+") 
+    for l in entrylog:
+      pieces = l.split(" ")
+      if pieces[0] == bucketname and pieces[1] == objname:
+        if pieces[2].strip() == "COMPLETE":
+          return "COMPLETE"
         else:
-          if id == i: 
-            path = '%s/%s-%d.obj' % (self.path, groupname, id)
-            f = open(path, "r")
-            d = pickle.loads(f.read())
-            f.close()
-            return d
-    elif isinstance(id, tuple):
-      for i in self.entries[groupname]:
-        if isinstance(i, tuple):
-          (start, end) = i
-          (tstart, tend) = id
-          if tstart >= start and tend <= end:
-            path = '%s/%s-(%d,%d)' % (self.path, groupname, start, end)   
-            f = open(path, "r")
-            d = pickle.loads(f.read())
-            f.close()
-            return d[tstart:(tend + 1)] 
-    else:
-      return None
+          foundDownload = True
+    if foundDownload:
+      return "downloading..."
+    return None 
+   
+  def __lockOrNone(self, bucketname, obj):
+    entrylog = open(self.path+'/cache.log', "a+") 
+    fcntl.flock(entrylog.fileno(), fcntl.LOCK_EX)
+    state = self.__getStateFromLog(bucketname, obj)
+    if state is None:
+      self.__log(bucketname, obj, "downloading...")
+      fcntl.flock(entrylog.fileno(), fcntl.LOCK_UN)
+      return "downloading..."
+    fcntl.flock(entrylog.fileno(), fcntl.LOCK_UN)
+    entrylog.close()
+    return None 
+
+  def __logWithLock(self, bucketname, objname, state):
+    entrylog = open(self.path+'/cache.log', "a+") 
+    fcntl.flock(entrylog.fileno(), fcntl.LOCK_EX)
+    entry = "%s %s %s" % (bucketname, objname, state)
+    entrylog.write(entry+'\n')    
+    fcntl.flock(entrylog.fileno(), fcntl.LOCK_UN)
+    entrylog.close()
+
+  def __log(self, bucketname, objname, state):
+    entrylog = open(self.path+'/cache.log', "a+") 
+    entry = "%s %s %s" % (bucketname, objname, state)
+    entrylog.write(entry+'\n')   
+    entrylog.close() 
