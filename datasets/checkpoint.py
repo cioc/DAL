70495e65ab5d743a62ea8cade400d1943d239384
Charles Cary <cclego@gmail.com>
2013-04-02 21:29:19 +0000

    checkpoint updates

--- datasets/checkpoint.py
+++ datasets/checkpoint.py
@@ -15,26 +15,36 @@ class Checkpoint:
     self.access_key = self.config['cache']['AWS_ACCESS_KEY']
     self.secret_key = self.config['cache']['AWS_SECRET_KEY']
      
-  def store(self, key, obj):
+  def store(self, key, obj=None, s=None, fp=None):
+    if all([obj is None, s is None, fp is None]):
+      raise Exception("o, s, or f must be set")
     conn = boto.connect_s3(self.access_key, self.secret_key)
     bucket = conn.get_bucket(self.bucket)
     k = Key(bucket)
     k.key = key
-    k.set_contents_from_string(pickle.dumps(obj))
+    if obj is not None:
+      k.set_contents_from_string(pickle.dumps(obj))
+    elif s is not None:
+      k.set_contents_from_string(s)
+    else:
+      k.set_contents_from_file(fp)
     conn.close()
 
-  def load(self, key):
+  def load(self, key, t=None):
     conn = boto.connect_s3(self.access_key, self.secret_key)
     bucket = conn.get_bucket(self.bucket)
     k = Key(bucket)
     k.key = key
-    o = pickle.loads(k.get_contents_as_string())
+    if t is not None and t == 'obj':
+      o = pickle.loads(k.get_contents_as_string())
+    else:
+      o = k.get_contents_as_string()
     conn.close()
     return o
 
   def list(self):
     conn = boto.connect_s3(self.access_key, self.secret_key)
     bucket = conn.get_bucket(self.bucket)
-    o = bucket.list() 
+    for i in bucket.list(): 
+      yield i.name
     conn.close()
-    return o
